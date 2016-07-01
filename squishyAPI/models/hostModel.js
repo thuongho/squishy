99a8ee1ae2a2473d90f102791b36dc6b22f69c72
Thuong Ho <thuong.t.ho@gmail.com>
2016-07-01 10:59:25 -0700

    add headers and update Schema

--- squishyAPI/models/hostModel.js
+++ squishyAPI/models/hostModel.js
@@ -6,10 +6,14 @@
         Schema = mongoose.Schema;
 
     var hostModel = new Schema({
-        facebooId: {type: String},
+        socialId: {type: String, index: true, unique: true},
+        username: {type: String, unique: true},
         firstName: {type: String},
         lastName: {type: String},
-        email: {type: String}
+        email: {type: String},
+        deviceToken: {type: String, unique: true},
+        deviceRegistered: {type: Boolean, default: false},
+        createdAt: {type: Date, default: Date.now}
     });
 
     module.exports = mongoose.model('Host', hostModel);
--- squishyAPI/server.js
+++ squishyAPI/server.js
@@ -20,6 +20,21 @@
 
     var port = process.env.PORT || 3000;
 
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header(
+            'Access-Control-Allow-Headers',
+            'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
+        if ('OPTIONS' === req.method) {
+            res.status(200).end();
+        } else {
+            next();
+        }
+    });
+
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
